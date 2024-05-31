cacf5a193b177de1d9f0276a04583ace1a0101cc
Olabode Omoyele <yele.olabode@gmail.com>
2024-05-31 09:16:34 +0100

    fix

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
     queryKey: ["products"],
     queryFn: async () => {
       const { data } = await axios.post<QueryResult<TProduct>[]>(
-        "http://localhost:3000/api/products",
+        "https://super-filter.vercel.app/api/products",
         {
           filter: {
             sort: filter.sort,
